bec8a094c746cd8578d3982c9695d6d234a7492f
Sami353 <malisami878@gmail.com>
2022-09-06 19:42:07 +0545

    Commiting Changes

--- wordle.py
+++ wordle.py
@@ -4,20 +4,25 @@ from words import words
 from rich.prompt import Prompt
 from rich.console import Console
 
+
 def get_words(word):
     word = random.choice(word)
     while len(word) != 5:
-        word =random.choice(word)
+        word = random.choice(word)
     return word.upper()
+
 
 def correct_position(letter):
     return f'[white on green]{letter}[/]'
 
+
 def correct_letter(letter):
     return f'[white on yellow]{letter}[/]'
 
+
 def incorrect_letter(letter):
     return f'[white on black]{letter}[/]'
+
 
 def score(guess, answer):
     score = []
@@ -30,6 +35,21 @@ def score(guess, answer):
             score += incorrect_letter(letter)
     return ''.join(score)
 
+
+def check_word(word):
+    if len(word) != 5:
+        return True
+    else:
+        return False
+
+
+def verify_word(word, list):
+    if word.lower() not in list:
+        return True
+    else:
+        return False
+
+
 def main():
     rich.print(correct_position('WELCOME') + " " + correct_letter('TO') + " " + incorrect_letter('WORDLE') + "")
 
@@ -41,12 +61,21 @@ def main():
     while guesses != 0:
         guesses -= 1
         player_input = Prompt.ask('Enter a Word').upper()
-        
+
+        while check_word(player_input):
+            console.print('This word is not valid. Please enter a valid word.')
+            player_input = Prompt.ask('Enter a valid word').upper()
+
+        while verify_word(player_input, words):
+            console.print('This word is not valid. Please enter a valid word.')
+            player_input = Prompt.ask('Enter a valid word').upper()
+
         console.print(score(player_input, actual_answer) + "")
         if player_input == actual_answer:
             console.print('CONGRATS! You guessed the word.')
             break
-    if guesses == 0 and player_input != 0:
+
+    if guesses == 0 and player_input != actual_answer:
         console.print('WRONG GUESS! The correct word is ' + correct_position(actual_answer))
 
 if __name__ == '__main__':
