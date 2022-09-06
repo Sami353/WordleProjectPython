47f456737c2791e0eeb5c38dcf634245844d33ec
Sami353 <malisami878@gmail.com>
2022-09-06 14:48:35 +0545

    Added code

---
+++ wordle.py
@@ -0,0 +1,53 @@
+import random
+import rich
+from words import words
+from rich.prompt import Prompt
+from rich.console import Console
+
+def get_words(word):
+    word = random.choice(word)
+    while len(word) != 5:
+        word =random.choice(word)
+    return word.upper()
+
+def correct_position(letter):
+    return f'[white on green]{letter}[/]'
+
+def correct_letter(letter):
+    return f'[white on yellow]{letter}[/]'
+
+def incorrect_letter(letter):
+    return f'[white on black]{letter}[/]'
+
+def score(guess, answer):
+    score = []
+    for index, letter in enumerate(guess):
+        if answer[index] == guess[index]:
+            score += correct_position(letter)
+        elif letter in answer:
+            score += correct_letter(letter)
+        else:
+            score += incorrect_letter(letter)
+    return ''.join(score)
+
+def main():
+    rich.print(correct_position('WELCOME') + " " + correct_letter('TO') + " " + incorrect_letter('WORDLE'))
+
+    guesses = 6
+
+    console = Console()
+    actual_answer = get_words(words)
+
+    while guesses != 0:
+        guesses -= 1
+        player_input = Prompt.ask('Enter a Word: ').upper()
+        
+        console.print(score(player_input, actual_answer))
+        if player_input == actual_answer:
+            console.print('CONGRATS! You guessed the correct word.')
+            break
+    if guesses == 0 and player_input != 0:
+        console.print('WRONG GUESS! The correct word is ' + (actual_answer))
+
+if __name__ == '__main__':
+    main()
